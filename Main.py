5f378b789137e03a8074e39b06c3ff73e2282f25
Dylan Telford <dylantelford@gmail.com>
2016-10-26 13:03:43 -0400

    Add clean_and_tag and trim_tokens method to make clean, tagged tokens from url

--- Main.py
+++ Main.py
@@ -3,8 +3,14 @@ Debate analyzer project
 
 Preston Mackert, Dylan Telford, Alexis Grebenok, Jerry Daigler
 """
-from __future__ import print_function
+
 import sys
+import nltk
+import re
+import bs4
+from __future__ import print_function
+from urllib import urlopen
+from bs4 import BeautifulSoup
 
 
 def main_menu():
@@ -24,4 +30,19 @@ def main():
           "exit the program")
     main_menu()
 
-main()
+def clean_and_tag(url):
+    """"""
+    raw = urlopen(url).read()
+    soup = BeautifulSoup(raw, 'html.parser')
+    cleaned = BeautifulSoup.get_text(soup)
+    tokens = nltk.word_tokenize(cleaned)
+    tokens = trim_tokens(tokens)
+    tagged_tokens = nltk.pos_tag(tokens)
+    return nltk.Text(tagged_tokens)
+
+def trim_tokens(tokens):
+    """Removes garbage from beginning and end of tokens"""
+
+
+if __name__ == '__main__':
+    main()
